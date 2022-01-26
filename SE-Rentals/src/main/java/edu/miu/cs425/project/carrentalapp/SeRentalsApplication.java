270e6773a98dcfa8eaf36dd6dd4b6d6def25c75c
dgebretsadik <gebredanab@gmail.com>
2022-01-26 11:41:51 -0600

    feat:Initial commit

---
+++ SE-Rentals/src/main/java/edu/miu/cs425/project/carrentalapp/SeRentalsApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.project.carrentalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeRentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeRentalsApplication.class, args);
+	}
+
+}
---
+++ SE-Rentals/src/test/java/edu/miu/cs425/project/carrentalapp/SeRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.project.carrentalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeRentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
